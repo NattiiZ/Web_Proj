6bbb23b25da41acc532e1fddcbfd79754164b1f9
Guntinan1134 <guntinanprosri@gmail.com>
2025-03-07 17:24:13 +0700

    modified:   upcoming.php

--- upcoming.php
+++ upcoming.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>โปรแกรมฉายหนัง</title>
+    <title>โปรแกรมหน้า</title>
     
     <!-- CSS -->
     <link rel="stylesheet" href="styles.css">
@@ -43,7 +43,7 @@
     </div>
 
     <div class="content">
-        <h1>โปรแกรมหนัง</h1>
+        <h1>โปรแกรมหน้า</h1>
         <div class="movies">
             <div class='movie'>
                 <img src='photo/novocaine.jpg' alt='ภาพยนตร์เรื่องที่ 1'>
